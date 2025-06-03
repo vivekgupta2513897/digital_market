bf7ba593aedb8ca9a9541cd0f8c1cf06cab0ecd9
VIVEK GUPTA <77668769+vivekgupta2513897@users.noreply.github.com>
2025-06-04 01:06:27 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-preset-env': {},
+    'postcss-image-set-polyfill': {},
+  },
+} 
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
+})
